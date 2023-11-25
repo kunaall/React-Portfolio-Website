a856acbe48c3b51146c19a1f6bb886c6eb69d296
kunaall <60544623+kunaall@users.noreply.github.com>
2023-11-25 14:25:51 +0530

    Add files via upload

---
+++ src/data/data.js
@@ -0,0 +1,29 @@
+import Brettman from '../assets/projects/brettman.png'
+import Socialbuzz from '../assets/projects/buzzsocialmedia.png'
+import Ecommercee from '../assets/projects/ecommerce.png'
+
+export const data=[
+    {
+        id:1,
+        name:"Ecommerce App",
+        image:Ecommercee,
+        github:"https://github.com/kunaall/Ecommerce-App--Sureshop-India.git",
+        live:"https://ecommerceappfrontend.onrender.com",
+    },
+    {
+        id:2,
+        name:"Library Management System",
+        image:Brettman,
+        github:"https://github.com/kunaall/BrettMan-Library-Management.git",
+        live:"https://brettmanlibarymanagement.onrender.com",
+    },
+    {
+        id:3,
+        name:"Social Media App",
+        image:Socialbuzz,
+        github:"https://github.com/kunaall/Buzztalk-Socialmedia-App.git",
+        live:"https://buzztalksocialapp.onrender.com",
+    }
+
+
+]
